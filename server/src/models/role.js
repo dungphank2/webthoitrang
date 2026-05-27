ff852cafbc6fb0b2d8027210d06341afc078c31a
Nguyễn Thành Đạt 20130225 <116094166+nguyenthanhdat20130225@users.noreply.github.com>
2026-05-27 16:37:48 +0700

    customer models

---
+++ server/src/models/role.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+
+const { sequelize } = require("../configs/database");
+
+const Role = sequelize.define(
+  "Role",
+  {
+    role_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    role_name: { type: DataTypes.STRING, allowNull: false, unique: true },
+  },
+  {
+    tableName: "roles",
+    timestamps: false,
+  }
+);
+
+module.exports = Role;
