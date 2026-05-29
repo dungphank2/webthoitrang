f64fd61a81f4d419a431cb404cc097a8ad142a41
Nguyễn Thành Đạt 20130225 <116094166+nguyenthanhdat20130225@users.noreply.github.com>
2026-05-30 00:35:45 +0700

    add admin static data
     thêm dữ liệu tỉnh cho admin

---
+++ admin-client/data/data.js
@@ -0,0 +1,27 @@
+export const menu = [
+  {
+    title: "Thống kê",
+    href: "/overview",
+    list: [],
+  },
+  {
+    title: "Quản lý danh mục",
+    href: "/catalog/manage",
+    list: [],
+  },
+  {
+    title: "Quản lý sản phẩm",
+    href: "/product/manage",
+    list: [],
+  },
+  {
+    title: "Quản lý đơn hàng",
+    href: "/order/manage",
+    list: [],
+  },
+  {
+    title: "Quản lý người dùng",
+    href: "/user/manage",
+    list: [],
+  },
+];
