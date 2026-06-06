c1bc55c8ffa778065a3d08cfa8bfe31d89337876
Nguyễn Thành Đạt 20130225 <116094166+nguyenthanhdat20130225@users.noreply.github.com>
2026-06-06 08:19:00 +0700

    add order service
    Thêm service gọi API đơn hàng

---
+++ web-client/services/orderService.js
@@ -0,0 +1,26 @@
+import axiosClient from "./axiosClient";
+import axiosJWT from "./axiosJWT";
+
+const orderService = {
+  getOrderHistory: async () => {
+    return await axiosJWT.get("/order/customer/list");
+  },
+
+  getDetail: async (orderId) => {
+    return await axiosJWT.get(`/order/detail/${orderId}`);
+  },
+
+  placeOrder: async (data) => {
+    return await axiosJWT.post("/order/create", data);
+  },
+
+  vnpayOrder: async (data) => {
+    return await axiosJWT.post("/order/create-payment-url", data);
+  },
+
+  cancelOrder: async (orderId) => {
+    return await axiosClient.put(`/order/change-status/${orderId}/5`);
+  },
+};
+
+export default orderService;
